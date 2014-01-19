cdab6d46b51b83a7482d167aec4e6a7500b2120f
mcerdeira <martincerdeira@gmail.com>
2014-01-19 19:53:11 -0300

    Initial commit

---
+++ AirDB/AirDbSvc.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AirDB
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Single)]
+    public class AirDBSvc : IAirDBSvc
+    {
+        private Dictionary<string, string> MainOBJ = new Dictionary<string, string>();
+
+        public Dictionary<string, string> getColletion()
+        {
+            return MainOBJ;
+        }
+
+        public string getValue(string key)
+        {
+            return MainOBJ[key];
+        }
+
+        public string getKey(string value)
+        {
+            return MainOBJ.FirstOrDefault(x => x.Value == value).Key;
+        }
+
+        public void setKeyValue(string key, string value)
+        {
+            MainOBJ.Add(key, value);
+        }
+
+        public void removeKey(string key)
+        {
+            MainOBJ.Remove(key);            
+        }
+
+        public void clear()
+        {
+            MainOBJ.Clear();
+        }
+    }
+}
---
+++ AirDB/IAirDBSvc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AirDB
+{    
+    [ServiceContract]    
+    public interface IAirDBSvc
+    {
+        [OperationContract]
+        Dictionary<string, string> getColletion();
+
+        [OperationContract]
+        string getValue(string key);
+
+        [OperationContract]
+        string getKey(string value);
+
+        [OperationContract]
+        void setKeyValue(string key, string value);
+
+        [OperationContract]
+        void removeKey(string key);
+
+        [OperationContract]
+        void clear();
+    }
+}
